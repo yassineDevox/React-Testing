83e97d0cf8183af101f079d2582f76ff7bf8bae7
yassineDevox <yassine.rassy1@gmail.com>
2022-11-23 15:39:38 +0100

    add test for prop name of hello component

--- app/src/components/hello/hello.test.tsx
+++ app/src/components/hello/hello.test.tsx
@@ -6,3 +6,10 @@ test('Hello renders correctly',()=>{
    const textElement = screen.getByText(/hello world 😇!/i)
    expect(textElement).toBeInTheDocument()
 })
+
+test('Hello renders with a name',()=>{
+   render(<Hello name="yassine"/>)
+   const textElement = screen.getByText(/hello yassine 😇!/i)
+   expect(textElement).toBeInTheDocument()
+})
+
