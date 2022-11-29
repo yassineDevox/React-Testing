610ffbfe32484a89854ee1dba6cee930bcb7b478
yassineDevox <yassine.rassy1@gmail.com>
2022-11-29 16:44:32 +0100

    done

--- app/src/components/skills/skills.test.tsx
+++ app/src/components/skills/skills.test.tsx
@@ -1,17 +1,27 @@
 import { screen, render } from "@testing-library/react"
 import Skills from "./skills"
 
-describe("skills",()=>{
-    const items = ["hacker","dev","designer"]
-    test("renders with props ",()=>{
-        render(<Skills items={items}/>)
+describe("skills", () => {
+    const items = ["hacker", "dev", "designer"]
+    test("renders with props ", () => {
+        render(<Skills items={items} />)
         const skillsEl = screen.getByRole("list")
         expect(skillsEl).toBeInTheDocument()
     })
-    test("renders the list of items ",()=>{
-        render(<Skills items={items}/>)
+    test("renders the list of items ", () => {
+        render(<Skills items={items} />)
         const skillsItems = screen.getAllByRole("listitem")
         expect(skillsItems).toHaveLength(items.length)
+    })
+    test("renders the login button", () => {
+        render(<Skills items={items} />)
+        const loginBtn = screen.getByRole("button", { name: "login ⚠️" })
+        expect(loginBtn).toBeInTheDocument()
+    })
+    test("not renders the start learning button", () => {
+        render(<Skills items={items} />)
+        const startLearningBtn = screen.queryByRole("button", { name: "Start Learning 😇" })
+        expect(startLearningBtn).not.toBeInTheDocument()
     })
 })
 
--- app/src/components/skills/skills.tsx
+++ app/src/components/skills/skills.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface StringProps {
     items: String[]
 }
 
 const Skills = (props: StringProps) => {
-    return <ul>
-        {props.items.map((item, i) => <li key={i}>{item}</li>)}
-    </ul>
 
+const [logged,loggedIn] = useState(false)
+return <div>
+    { logged ? <button>Start Learning 😇</button>:<button>login ⚠️</button> }
+   <ul>
+        {props.items.map((item, i) => <li key={i}>{item}</li>)}
+    </ul> 
+</div>
+
+    
 }
 
 export default Skills
