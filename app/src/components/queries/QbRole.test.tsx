a435d9e1cd95a1938e505cf4b1570c02a12eb1e5
yassineDevox <yassine.rassy1@gmail.com>
2022-11-29 14:37:49 +0100

    done

--- app/src/components/queries/QbRole.test.tsx
+++ app/src/components/queries/QbRole.test.tsx
@@ -2,20 +2,31 @@ import { screen, render } from "@testing-library/react"
 import React from "react"
 import QbRole from "./QbRole"
 
-describe("Query by role component", () => {
+describe("Query by role component & options", () => {
 
     test('renders', () => {
         render(<QbRole />)
+        //testbox
+        const nameEl = screen.getByRole("textbox", { name: "Name" })
         //select
-        const selectContriesEl =  screen.getByRole("listContries")
+        const selectContriesEl = screen.getByRole("listContries")
         //checkbox 
-        const checkboTerms =  screen.getByRole("checkbox")
+        const checkboTerms = screen.getByRole("checkbox")
         //btn
         const btnSubmitEl = screen.getByRole("button") as HTMLButtonElement
+        //heading h1 & h2
+        const h1El = screen.getByRole("heading", { level: 1 })
+        const h2El = screen.getByRole("heading", { level: 2 })
         //expect
         expect(btnSubmitEl).toHaveTextContent(/submit/i)
         expect(selectContriesEl).toBeInTheDocument()
         expect(checkboTerms).toBeInTheDocument()
+        expect(h1El).toBeInTheDocument()
+        expect(h2El).toBeInTheDocument()
+        expect(nameEl).toBeInTheDocument()
+
     })
+
+
 
 })
