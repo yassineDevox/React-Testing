3edd5b913c94dddb0a6b48c5a95d962ab8d73c82
yassineDevox <yassine.rassy1@gmail.com>
2022-11-30 12:58:29 +0100

    done

--- app/src/components/counter/counter.test.tsx
+++ app/src/components/counter/counter.test.tsx
@@ -2,29 +2,42 @@ import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import Counter from "./counter"
 
-describe("counter",()=>{
-  
-  test("renders correctly",()=>{
+describe("counter", () => {
+
+  test("renders correctly", () => {
     render(<Counter />)
-    const counterValEl = screen.getByRole("heading",{level:1})
+    const counterValEl = screen.getByRole("heading", { level: 1 })
     expect(counterValEl).toBeInTheDocument()
-    const incrementBtn = screen.getByRole("button",{name:/increment/i})
+    const incrementBtn = screen.getByRole("button", { name: /increment/i })
     expect(incrementBtn).toBeInTheDocument()
   })
 
-  test("renders with counter equal to 0",()=>{
-    render(<Counter/>)
-    const counterValEl = screen.getByRole("heading",{level:1})
+  test("renders with counter equal to 0", () => {
+    render(<Counter />)
+    const counterValEl = screen.getByRole("heading", { level: 1 })
     expect(counterValEl).toHaveTextContent("0")
   })
 
-  test("renders with counter val equal to 1 after clicking on increment button",async ()=>{
+  test("renders with counter val equal to 1 after clicking on increment button", async () => {
     const user = userEvent.setup()
-    render(<Counter/>)
-    const counterValEl = screen.getByRole("heading",{level:1})
-    const counterBtn = screen.getByRole("button",{name:/increment/i})
+    render(<Counter />)
+    const counterValEl = screen.getByRole("heading", { level: 1 })
+    const counterBtn = screen.getByRole("button", { name: /increment/i })
     await user.click(counterBtn)
     expect(counterValEl).toHaveTextContent("1")
   })
 
+  test("renders with count of 10 after clicking the set button", async () => {
+    const user = userEvent.setup()
+    render(<Counter />)
+    const amountInpt = screen.getByRole("spinbutton")
+    expect(amountInpt).toBeInTheDocument()
+    await user.type(amountInpt, "10")
+    expect(amountInpt).toHaveValue(10)
+    const setCounterBtn = screen.getByRole("button", { name: /set/i })
+    await user.click(setCounterBtn)
+    const counterValEl = screen.getByRole("heading", { level: 1 })
+    expect(counterValEl).toHaveTextContent("10")
+
+  })
 })
--- app/src/components/counter/counter.tsx
+++ app/src/components/counter/counter.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 
 const Counter = () => {
-    const [counter, setcounter] = useState(0)
-    const onIncrement = () => setcounter(c => c + 1)
+    const [counter, setCounter] = useState(0)
+    const [amount, setAmount] = useState(0)
+    const onIncrement = () => setCounter(c => c + 1)
     return (
         <div>
             <h1>{counter}</h1>
             <button onClick={onIncrement}> Increment 😇</button>
+            <input
+                type="number"
+                placeholder="add amount"
+                onChange={(e) => setAmount(parseInt(e.target.value))} />
+            <button onClick={_ => setCounter(amount)}>Set</button>
         </div>
     )
 }
